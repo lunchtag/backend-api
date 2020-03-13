5758f4e81852ec1ff2b897fe340f71d0ae72fced
Nguyen,Karin K.T <karin.nguyen@student.fontys.nl>
2020-03-13 17:55:58 +0100

    users in admin

--- src/main/java/nl/lunchtag/resource/Lunchtag/DOMAIN/Models/Admin.java
+++ src/main/java/nl/lunchtag/resource/Lunchtag/DOMAIN/Models/Admin.java
@@ -1,11 +1,15 @@
 package nl.lunchtag.resource.Lunchtag.DOMAIN.Models;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Admin {
 
     private long adminID;
     private String username;
     private String password;
     private String repeatedPassword;
+    private Set<User> users = new HashSet<>();
 
     public Admin(long adminID, String username, String password) {
         this.adminID = adminID;
@@ -13,5 +17,22 @@ public class Admin {
         this.password = password;
     }
 
+
+    public User getUserByID(long userID)
+    {
+        User user = users.stream()
+                .filter( user1 -> userID == user1.getUserID())
+                .findFirst()
+                .orElse(null);
+        return user;
+    }
+    public void addUser(User user)
+    {
+        users.add(user);
+    }
+    public void removeUser(long userID)
+    {
+        users.remove(getUserByID(userID));
+    }
 
 }
--- src/main/java/nl/lunchtag/resource/Lunchtag/DOMAIN/Models/User.java
+++ src/main/java/nl/lunchtag/resource/Lunchtag/DOMAIN/Models/User.java
@@ -1,4 +1,25 @@
 package nl.lunchtag.resource.Lunchtag.DOMAIN.Models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class User {
+    private long userID;
+    private String name;
+    private String lastname;
+    private String email;
+    private String username;
+    private String password;
+    private String repeatedPassword;
+
+    public User(long userID, String name, String lastname, String email, String username, String password) {
+        this.userID = userID;
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
 }
