4d55aabc15999d4687b7bf8384fc4c3c72bce76c
Nguyen,Karin K.T <karin.nguyen@student.fontys.nl>
2020-03-13 17:36:16 +0100

    add notification and lunch

--- src/main/java/nl/lunchtag/resource/Lunchtag/DOMAIN/Models/Account.java
+++ src/main/java/nl/lunchtag/resource/Lunchtag/DOMAIN/Models/Account.java
@@ -1,4 +1,40 @@
 package nl.lunchtag.resource.Lunchtag.DOMAIN.Models;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Account {
+    private long accountID;
+    private String name;
+    private String lastname;
+    private String email;
+    private String username;
+    private String password;
+    private String repeatedPassword;
+    private Set<Lunch> lunches = new HashSet<>();
+
+    public Account(long accountID, String name, String lastname, String email, String username, String password) {
+        this.accountID = accountID;
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public void addLunch(Lunch lunch)
+    {
+        lunches.add(lunch);
+    }
+    public Lunch getLunchByID(long lunchID)
+    {
+        Lunch lunch = lunches.stream()
+                .filter(lunch1 -> lunchID == (lunch1.getLunchID()))
+                .findFirst()
+                .orElse(null);
+        return lunch;
+    }
+
+
+
 }
--- src/main/java/nl/lunchtag/resource/Lunchtag/DOMAIN/Models/Lunch.java
+++ src/main/java/nl/lunchtag/resource/Lunchtag/DOMAIN/Models/Lunch.java
@@ -1,4 +1,13 @@
 package nl.lunchtag.resource.Lunchtag.DOMAIN.Models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+@Getter
+@Setter
 public class Lunch {
+    private long lunchID;
+    private Date date;
 }
