d89145fe05e9fdca95c6bda07b5cfd36aabe59d6
Nguyen,Karin K.T <karin.nguyen@student.fontys.nl>
2020-03-13 18:03:00 +0100

    add admin functions

--- src/main/java/nl/lunchtag/resource/Lunchtag/DOMAIN/Models/Admin.java
+++ src/main/java/nl/lunchtag/resource/Lunchtag/DOMAIN/Models/Admin.java
@@ -10,6 +10,7 @@ public class Admin {
     private String password;
     private String repeatedPassword;
     private Set<User> users = new HashSet<>();
+    private Set<Notification> notifications = new HashSet<>();
 
     public Admin(long adminID, String username, String password) {
         this.adminID = adminID;
@@ -18,21 +19,42 @@ public class Admin {
     }
 
 
-    public User getUserByID(long userID)
-    {
+    public User getUserByID(long userID) {
         User user = users.stream()
-                .filter( user1 -> userID == user1.getUserID())
+                .filter(user1 -> userID == user1.getUserID())
                 .findFirst()
                 .orElse(null);
         return user;
     }
-    public void addUser(User user)
-    {
+
+    public void addUser(User user) {
         users.add(user);
     }
-    public void removeUser(long userID)
-    {
+
+    public void removeUser(long userID) {
         users.remove(getUserByID(userID));
     }
+
+    public Set<User> getAllUsers() {
+        return users;
+    }
+
+    public void createNotification(Notification notification) {
+        notifications.add(notification);
+    }
+
+    public Notification getNotificationByID(long notificationID) {
+        Notification notification = notifications.stream()
+                .filter(notification1 -> notificationID == notification1.getNotificationID())
+                .findFirst()
+                .orElse(null);
+        return notification;
+    }
+
+    public void removeNotifcationByID(long notificationID)
+    {
+        notifications.remove(getNotificationByID(notificationID));
+    }
+
 
 }
