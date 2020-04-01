bf57d7bc4190db0a366c61155f79a95447afe088
Manders,Rens R.A <r.manders@student.fontys.nl>
2020-04-01 10:30:56 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/nl/lunchtag/resource/Lunchtag/WEB/CONFIG/SecurityConfiguration.java

--- src/main/java/nl/lunchtag/resource/Lunchtag/WEB/CONFIG/SecurityConfiguration.java
+++ src/main/java/nl/lunchtag/resource/Lunchtag/WEB/CONFIG/SecurityConfiguration.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
@@ -41,6 +38,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .authorizeRequests()
                     .antMatchers("/h2/**").permitAll()
                     .antMatchers("/console/**").permitAll()
+                    .antMatchers("/swagger-ui.html/**").permitAll()
+                    .antMatchers("/api/**").permitAll()
+                    .antMatchers("/swagger-resources/**").permitAll()
+                    .antMatchers("/v2/api-docs/**").permitAll()
+                    .antMatchers("/webjars/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
                 .apply(new TokenConfigurer(tokenProvider));
