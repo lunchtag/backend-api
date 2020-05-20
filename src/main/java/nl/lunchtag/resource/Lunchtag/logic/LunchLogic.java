68748c418ea1a4240569cb34730cdd56ce183a9a
Julian Lu <j.lu@student.fontys.nl>
2020-05-20 11:23:02 +0200

    PDF sends to the client instead of a URL.

--- src/main/java/nl/lunchtag/resource/Lunchtag/logic/LunchLogic.java
+++ src/main/java/nl/lunchtag/resource/Lunchtag/logic/LunchLogic.java
@@ -1,5 +1,10 @@
 package nl.lunchtag.resource.Lunchtag.logic;
 
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfName;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import nl.lunchtag.resource.Lunchtag.entity.Account;
 import nl.lunchtag.resource.Lunchtag.entity.Lunch;
 import nl.lunchtag.resource.Lunchtag.models.LunchDTO;
@@ -8,6 +13,7 @@ import nl.lunchtag.resource.Lunchtag.service.LunchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.*;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -17,11 +23,13 @@ public class LunchLogic {
 
     private final LunchService lunchService;
     private final AccountService accountService;
+    private final ExportPdfLogic exportPdfLogic;
 
     @Autowired
-    public LunchLogic(LunchService lunchService, AccountService accountService) {
+    public LunchLogic(LunchService lunchService, AccountService accountService,ExportPdfLogic exportPdfLogic) {
         this.lunchService = lunchService;
         this.accountService = accountService;
+        this.exportPdfLogic = exportPdfLogic;
     }
 
     public Lunch addLunch(Account account, LunchDTO lunchDTO) {
@@ -56,5 +64,66 @@ public class LunchLogic {
 
     public Optional<Lunch> findById(UUID id) {
         return this.lunchService.findById(id);
+    }
+
+    public byte[] generatePdf(int year,int month){
+        List<Lunch> lunches = lunchService.findAll();
+        List<Lunch> filteredLunches = exportPdfLogic.returnLunchesOfMonth(month,year,lunches);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        Document document = new Document();
+
+        try{
+            PdfWriter writer = PdfWriter.getInstance(document,byteArrayOutputStream);
+            document.open();
+            Font titleFont=new Font(Font.FontFamily.HELVETICA,25.0f,Font.BOLD, BaseColor.BLACK);
+            Font tableFont = new Font(Font.FontFamily.HELVETICA, 18.0f,Font.BOLD, BaseColor.BLACK);
+            // Title
+            Paragraph title = new Paragraph("Lunchtag - "+year+" Maandnummer " + (month+1),titleFont);
+            title.setAlignment(Paragraph.ALIGN_CENTER);
+
+            title.setSpacingAfter(25f);
+            document.add(title);
+
+            // Table
+            PdfPTable table = new PdfPTable(3);
+
+            // Create cell
+            PdfPCell c1 = new PdfPCell(new Paragraph("Voornaam",tableFont));
+            PdfPCell c2 = new PdfPCell(new Paragraph("Achternaam",tableFont));
+            PdfPCell c3 = new PdfPCell(new Paragraph("Datum",tableFont));
+
+            // Add cell to table
+            table.addCell(c1);
+            table.addCell(c2);
+            table.addCell(c3);
+
+            for(Lunch lunch : filteredLunches){
+                c1= new PdfPCell(new Paragraph(lunch.getAccount().getName()));
+                c2= new PdfPCell(new Paragraph(lunch.getAccount().getLastName()));
+                c3= new PdfPCell(new Paragraph(lunch.getDate().toString()));
+
+                table.addCell(c1);
+                table.addCell(c2);
+                table.addCell(c3);
+            }
+
+            // Add objects to pdf
+            document.add(table);
+
+            Paragraph lunchTitle = new Paragraph("Totaal aantal x meegelunched:" + filteredLunches.size(),titleFont);
+            lunchTitle.setAlignment(Paragraph.ALIGN_CENTER);
+            document.add(lunchTitle);
+
+            // Close
+            document.close();
+            writer.close();
+            byte[] pdfBytes = byteArrayOutputStream.toByteArray();
+
+            return pdfBytes;
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
