142847edc83cdfb2488d23a2a8c0f9d6f789ea8e
Julian Lu <j.lu@student.fontys.nl>
2020-05-20 14:26:33 +0200

    Changed text in PDF

--- src/main/java/nl/lunchtag/resource/Lunchtag/logic/LunchLogic.java
+++ src/main/java/nl/lunchtag/resource/Lunchtag/logic/LunchLogic.java
@@ -111,7 +111,7 @@ public class LunchLogic {
             // Add objects to pdf
             document.add(table);
 
-            Paragraph lunchTitle = new Paragraph("Totaal aantal x meegelunched:" + filteredLunches.size(),titleFont);
+            Paragraph lunchTitle = new Paragraph("Totaal "+ filteredLunches.size()+" keer meegelunched:" ,titleFont);
             lunchTitle.setAlignment(Paragraph.ALIGN_CENTER);
             document.add(lunchTitle);
 
